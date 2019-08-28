2fe81c8c5e353f064c6d4a23c0737262272e454d
da0ke <592052922@qq.com>
2019-08-28 13:43:52 +0800

    ud

--- IOSKit/Source/utils/RegexUtils.swift
+++ IOSKit/Source/utils/RegexUtils.swift
@@ -12,27 +12,31 @@ import Foundation
 public class RegexUtils {
     
     public static func isUsername(str:String) -> Bool {
-        return matches(str: str, pattern: "^[a-zA-Z0-9_]{4,20}$")
+        return isMatches(str: str, pattern: "^[a-zA-Z0-9_]{4,20}$")
     }
     
     public static func isPassword(str:String) -> Bool {
-        return matches(str: str, pattern: "^[^\\u4e00-\\u9fa5]{6,20}$")
+        return isMatches(str: str, pattern: "^[^\\u4e00-\\u9fa5]{6,20}$")
     }
     
     public static func isMobile(str:String) -> Bool {
-        return matches(str: str, pattern: "^1[3456789][0-9]{9}$");
+        return isMatches(str: str, pattern: "^1[3456789][0-9]{9}$");
     }
     
     public static func isTel(str:String) -> Bool {
-        return matches(str: str, pattern: "^[0-9-+]{6,}$")
+        return isMatches(str: str, pattern: "^[0-9-+]{6,}$")
     }
     
     public static func isEmail(str:String) -> Bool {
-        return matches(str: str, pattern: "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$")
+        return isMatches(str: str, pattern: "^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*$")
+    }
+    
+    public static func isUnsignedInteger(str: String) -> Bool {
+        return isMatches(str: str, pattern: "^(0|([1-9]\\d*))$");
     }
     
     
-    private static func matches(str:String, pattern:String) -> Bool {
+    public static func isMatches(str:String, pattern:String) -> Bool {
         let range = str.range(of: pattern, options: .regularExpression, range: str.startIndex..<str.endIndex, locale: .current);
         return range != nil;
     }
